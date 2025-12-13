23a0216f8bc2a080bc520de5c74334f93c0e41f8
daniilmiagkov <d.miagkov@gmail.com>
2025-12-14 01:29:48 +0300

    add loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    svgLoader()
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   css: {
     modules: {
-      localsConvention: 'camelCase'
+      localsConvention: 'camelCase',
     },
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/styles/variables.scss" as *;`
-      }
-    }
-  }
-})
-
+        additionalData: `@use "@/styles/variables.scss" as *;`,
+      },
+    },
+  },
+});
